222ac16c9a18d53b31afc0c7d0ddacd09c41be2c
umas <1970313791@qq.com>
2023-10-23 16:33:59 +0800

    fix

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # easyP
-常用表情包快速合成
+常用约饭表情包快速合成
 
 
 ## 添加新图
 
 - 基底图放在 public/base/xxx
 - 子图元放在 public/item/xxx
-- 手动更新 src/assets/index_xxx.json
+- 手动更新索引 src/assets/index_xxx.json
+  - 索引可以乱序，图片命名不影响调用（但推荐遵循格式（3位数字））
 
 
 ## 添加新的分类
@@ -15,3 +16,9 @@
 - HomeView.vue import json
 - HomeView.vue添加html
   - 注意修改name和图片路径
+
+
+## 更新记录
+
+- 2023.10.23 增加canvas绘图
+- 2023.8.1 大概是这一天创建的项目
--- easyp_page/src/views/HomeView.vue
+++ easyp_page/src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <h1>easyP 约饭速P</h1>
-    <h3>前端没有图像处理功能，图片调整好之后手动截图保存</h3>
 
     <!-- <el-button @click="test_button">test</el-button> -->
 
@@ -9,37 +8,40 @@
     <!-- 调节滑块 -->
 
     <div class="slider-box">
-      <div class="slider-item">
-        <span>尺寸-基底宽度</span>
-        <el-slider v-model="base_width" />
-      </div>
-      <div class="slider-item">
-        <span>尺寸-子元宽度</span>
-        <el-slider v-model="item_width" />
-      </div>
-      <div class="slider-item">
-        <span>尺寸-子元高度</span>
-        <el-slider v-model="item_height" />
-      </div>
-      <div class="slider-item">
-        <span>位置-子元top</span>
-        <el-slider v-model="item_top" />
-      </div>
-      <div class="slider-item">
-        <span>位置-子元left</span>
-        <el-slider v-model="item_left" />
-      </div>
+      <el-form label-width="150px">
+        <el-form-item label="整体宽度">
+          <el-slider v-model="box_width_percent" :min="10" :max="90" />
+        </el-form-item>
+        <el-form-item label="尺寸-基底宽度">
+          <el-slider v-model="base_width_percent" :min="10" :max="90" />
+        </el-form-item>
+        <el-form-item label="尺寸-子元宽度">
+          <el-slider v-model="item_width_percent" :min="5" :max="50" />
+        </el-form-item>
+        <el-form-item label="位置-子元top">
+          <el-slider v-model="item_top_persent" :min="0" :max="100" />
+        </el-form-item>
+        <el-form-item label="位置-子元left">
+          <el-slider v-model="item_left_persent" :min="0" :max="100" />
+        </el-form-item>
+      </el-form>
     </div>
 
 
-    <!-- 顶部成品图 -->
-
-    <div class="result">
-      <img class="base" :src="'base/' + base_select" alt="" :style="{ width: base_width + '%' }">
-      <div class="item-box"
-        :style="{ width: item_width + '%', height: item_height + '%', top: item_top + '%', left: item_left + '%' }">
-        <img :src="item_select" alt="">
+    <!-- 叠加成品图 -->
+    <div class="result-box">
+      <div class="img-box">
+        <img class="base" :src="'base/' + base_select" alt="" :style="{ width: base_width + 'px' }">
+        <img class="item" :src="item_select" alt=""
+          :style="{ width: item_width + 'px', top: item_top + 'px', left: item_left + 'px' }">
       </div>
+    </div>
+
+    <el-button style="margin: 20px;" type="primary" @click="paint_canvas">绘图</el-button>
+
+    <!-- canvas成品图 -->
+    <div class="canvas-box">
+      <canvas id="cv-res"></canvas>
     </div>
 
 
@@ -100,9 +102,8 @@
     <div>
       <h4>以下是一些以后可能会上线的功能（但更可能不会上线）</h4>
       <h6>1.鼠标拖动子图元调整位置和大小</h6>
-      <h6>2.前端直接生成合成后的图片</h6>
-      <h6>3.开一个图床存放更多的图</h6>
-      <h6>4.上传自定义图片</h6>
+      <h6>2.开一个图床存放更多的图</h6>
+      <h6>3.上传自定义图片</h6>
 
       <div>
         umas@2023
@@ -115,7 +116,7 @@
 
 
 <script lang="ts" setup>
-import { ref, Ref, onMounted } from "vue"
+import { ref, Ref, onMounted, computed } from "vue"
 import { ElMessage } from 'element-plus'
 
 
@@ -127,17 +128,17 @@ import index_item_other from "@/assets/index_item_other.json"
 
 // 显示模式切换(横屏/竖屏)
 const ishandy = ref(false)
-// 初始化图片宽度
+// 初始化图片宽度(横屏/竖屏)
 onMounted(() => {
-  console.log(window.innerWidth)
   if (window.innerWidth < 800) {
     ishandy.value = true
     let silder_box = window.document.querySelector(".slider-box");
     (silder_box as HTMLElement).style.width = "90%"
-    let result_box = window.document.querySelector(".result");
-    (result_box as HTMLElement).style.width = "90%"
+
     let collapse_box = window.document.querySelector(".el-collapse");
     (collapse_box as HTMLElement).style.width = "90%"
+
+    box_width_percent.value = 90
   }
 })
 
@@ -145,25 +146,91 @@ onMounted(() => {
 // 选中的图号
 const base_select = ref("001.jpg")
 const item_select = ref("item_eat/001.jpg")
-const set_base = (value:string)=>{
+const set_base = (value: string) => {
   base_select.value = value
-  let msg = "set"+JSON.stringify(value)
+  let msg = "set" + JSON.stringify(value)
+  get_aspect_ratio("base/" + base_select.value, base_ratio)
   ElMessage.success(msg)
 }
-const set_item = (value:string)=>{
+const set_item = (value: string) => {
   item_select.value = value
-  let msg = "set"+JSON.stringify(value)
+  let msg = "set" + JSON.stringify(value)
+  get_aspect_ratio("item/" + item_select.value, item_ratio)
   ElMessage.success(msg)
 }
 
+
+
+// 获取当前窗口的宽度，一切宽度都以此为基准计算出具体像素值（而不是百分比）
+const windowWidth = ref(window.innerWidth)
+
+// 整体宽度
+const box_width_percent = ref(40)
+const box_width = computed(() => Math.floor(windowWidth.value * box_width_percent.value / 100))
 
 // 宽度条件
-const base_width = ref(80)
-const item_width = ref(30)
-const item_height = ref(30)
+const base_width_percent = ref(50)
+const base_width = computed(() => Math.floor(base_width_percent.value / 100 * box_width.value))
+const item_width_percent = ref(20)
+const item_width = computed(() => Math.floor(item_width_percent.value / 100 * box_width.value))
+
+
 // 位置条件
-const item_top = ref(5)
-const item_left = ref(20)
+const item_left_persent = ref(0)
+const item_left = computed(() => Math.floor(item_left_persent.value / 100 * base_width.value))
+const item_top_persent = ref(0)
+const item_top = computed(() => Math.floor(item_top_persent.value / 100 * base_width.value * base_ratio.value))
+
+
+// 长宽比（长/宽）
+const base_ratio = ref(1)
+const item_ratio = ref(1)
+const get_aspect_ratio = (url: string, save_ref: Ref<any>) => {
+  let image = new Image()
+  image.src = url
+  image.onload = function () {
+    save_ref.value = image.height / image.width
+  }
+}
+
+
+const paint_canvas = () => {
+
+  const canvasBox = (document.querySelector(".canvas-box") as HTMLElement)
+  canvasBox.style.display = "block"
+
+  // 创建 Canvas 元素
+  var canvas = (document.querySelector("#cv-res") as HTMLCanvasElement)
+  var context = canvas!.getContext('2d');
+
+  // 加载第一张图片
+  var image1 = new Image();
+  image1.src = 'base/' + base_select.value
+
+  // 加载第二张图片
+  var image2 = new Image();
+  image2.src = item_select.value
+
+  // 等待两张图片加载完成
+  image1.onload = function () {
+    image2.onload = function () {
+      // 设置 Canvas 大小
+      canvas!.width = base_width.value
+      canvas!.height = Math.floor(base_width.value * base_ratio.value)
+
+      // 在 Canvas 上绘制第一张图片
+      context!.drawImage(image1, 0, 0, base_width.value, Math.floor(base_width.value * base_ratio.value));
+
+      // 在 Canvas 上绘制第二张图片（覆盖在第一张图片上）
+      context!.drawImage(image2, item_left.value, item_top.value, item_width.value, Math.floor(item_width.value * item_ratio.value));
+
+    };
+  };
+
+}
+
+
+
 
 
 // 测试按钮
@@ -189,6 +256,10 @@ div.home {
 // 滑块
 div.slider-box {
   width: 40%;
+  border: solid 2px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  margin: 30px;
+  padding-top: 20px;
 
   div.slider-item {
     display: flex;
@@ -201,42 +272,52 @@ div.slider-box {
 }
 
 // 顶部成品图
-div.result {
+div.result-box {
   position: relative;
   border: solid 2px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
   padding: 30px;
-  width: 40%;
   text-align: center;
+  margin: 30px;
 
-  .base {
+  div.img-box {
+    display: inline-block;
     position: relative;
-    width: 100%;
-  }
 
-  div.item-box {
-    position: absolute;
-    height: 40%;
-    width: 40%;
-    z-index: 99;
-
-    img {
+    .base {
+      position: relative;
       width: 100%;
-      height: 100%;
+    }
+
+    .item {
+      position: absolute;
+      z-index: 99;
     }
   }
+
+
 }
 
+
+// canvas成品
+div.canvas-box {
+  position: relative;
+  border: solid 2px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  padding: 30px;
+  text-align: center;
+  display: none;
+  margin: 30px;
+}
 
 // 折叠栏图元选择
 
 .el-collapse {
   width: 40%;
-  padding-top: 30px;
-  padding-bottom: 200px;
-  padding-left: 30px;
-  padding-right: 30px;
-  border-left: solid 1px rgba(0, 0, 0, 0.234);
-  border-right: solid 1px rgba(0, 0, 0, 0.234);
+  padding: 20px;
+  border: solid 2px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  margin: 30px;
 }
 
 
